1f3e4e1d06c0c27d92a039b0ce9c68985d289613
aayushi-aqua <aayushisingh6464@gmail.com>
2021-07-13 00:21:27 +0530

    styling

--- client/src/components/screens/RegisterScreen.js
+++ client/src/components/screens/RegisterScreen.js
@@ -53,57 +53,57 @@ const RegisterScreen = ({ history }) => {
   return (
     <div className="register-screen">
       <form onSubmit={registerHandler} className="register-screen__form">
-        <h3 className="register-screen__title">Register</h3>
+        <h3 className="register-screen__title">REGISTER</h3>
         {error && <span className="error-message">{error}</span>}
-        <div className="form-group">
-          <label htmlFor="name">Username:</label>
-          <input
-            type="text"
-            required
-            id="name"
-            placeholder="Enter username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+        <div className="form">
+          <div className="form-group">
+            <input
+              type="text"
+              required
+              id="name"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              required
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              required
+              id="password"
+              autoComplete="true"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              required
+              id="confirmpassword"
+              autoComplete="true"
+              placeholder="Confirm Password"
+              value={confirmpassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          <div className="button">
+            <button type="submit" className="btn btn-primary" id="btn">
+              Register
+            </button>
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            required
-            id="email"
-            placeholder="Email address"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            required
-            id="password"
-            autoComplete="true"
-            placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="confirmpassword">Confirm Password:</label>
-          <input
-            type="password"
-            required
-            id="confirmpassword"
-            autoComplete="true"
-            placeholder="Confirm password"
-            value={confirmpassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Register
-        </button>
 
         <span className="register-screen__subtext">
           Already have an account? <Link to="/login">Login</Link>
--- client/src/components/screens/drag.js
+++ client/src/components/screens/drag.js
@@ -9,7 +9,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import styles from "./index.module.css";
 import arrayMove from "./arrayMove";
 import axios from "axios";
-require("./navbar.css")
+require("./navbar.css");
 
 //Drag handler
 const DragHandle = sortableHandle(() => (
@@ -31,8 +31,6 @@ const SortableContainer = sortableContainer(({ children }) => {
   return <div className={styles.dragContainer}>{children}</div>;
 });
 
-
-
 class SortableItems extends Component {
   constructor(props) {
     super(props);
@@ -45,7 +43,6 @@ class SortableItems extends Component {
     this.check = this.check.bind(this);
     this.logoutHandler = this.logoutHandler.bind(this);
   }
-
 
   componentDidMount() {
     this.fetch();
@@ -61,12 +58,11 @@ class SortableItems extends Component {
 
       try {
         const element = await axios.post("/api/auth/getCode", config);
-       
-     
-        console.log(element)
+
+        console.log(element);
         this.setState({
-          level:  element.data.element.level,
-          items:  element.data.element.code,
+          level: element.data.element.level,
+          items: element.data.element.code,
           username: element.data.username,
           score: element.data.score,
         });
@@ -92,10 +88,10 @@ class SortableItems extends Component {
       const items = this.state.items;
       try {
         const element = await axios.post("/api/auth/check", { items }, config);
-       
+
         this.setState({
           level: element.data.element.level,
-          items:  element.data.element.code,
+          items: element.data.element.code,
           username: element.data.username,
           score: element.data.score,
         });
@@ -106,7 +102,7 @@ class SortableItems extends Component {
 
     check_code();
   }
-logoutHandler () {
+  logoutHandler() {
     localStorage.removeItem("authToken");
     window.location.href = "./login";
   }
@@ -115,30 +111,36 @@ logoutHandler () {
 
     return (
       <div>
-          <nav class="navbar">
-    <div class="logo">
-      <a href="/drag" class="navbar-brand animated flip">Web Pazuru </a>
-    </div>
-    <a href="/drag" class="toggle-button">
-      <span class="bar"></span>
-      <span class="bar"></span>
-      <span class="bar"></span>
-    </a>
-    <div class="navbar-links">
-      <ul>
-        <li><a href="/drag" >Rules</a></li>
-        <li><h1 id="roll">{username}</h1></li>
-        <li><button onClick={this.logoutHandler}>logout</button></li>
-      </ul>
-    </div>
-  </nav>
-        <div>
-         
-    
-        </div>
+        <nav className="navbar">
+          <div className="logo">
+            <a href="/drag" class="navbar-brand animated flip">
+              Web Pazuru{" "}
+            </a>
+          </div>
+          <a href="/drag" class="toggle-button">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </a>
+          <div className="navbar-links">
+            <ul>
+              <li>
+                <h1 id="roll">{username}</h1>
+              </li>
+              <li>
+                <a href="/drag">Rules</a>
+              </li>
+
+              <li>
+                <button onClick={this.logoutHandler}>logout</button>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div></div>
         <center>
-          <p > level : {level} </p>
-          <p > score:{score} </p>
+          <p> Level : {level} </p>
+          <p> Score:{score} </p>
         </center>
 
         <SortableContainer onSortEnd={this.onSortEnd} useDragHandle>
