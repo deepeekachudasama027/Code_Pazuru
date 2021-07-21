b7a3e3256e907c8c7f8475bfa06dce5316d16405
027DeepeekaChudasama <deepeekachudasama@gmail.com>
2021-07-21 17:36:50 +0530

    heroku

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI, {
+  await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1/web_pazuru", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
