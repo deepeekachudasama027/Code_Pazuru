e93dc98401065912e617942d4328e1f0e4ae1ecf
027DeepeekaChudasama <deepeekachudasama@gmail.com>
2021-10-03 22:30:42 +0530

    ques

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // Routing
 import PrivateRoute from "./components/routing/PrivateRoute";
 
